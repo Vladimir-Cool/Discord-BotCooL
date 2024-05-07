1e6a9d34963cc63810c29fec118a1a3425abf950
Vladimir <58716739+Vladimir-Cool@users.noreply.github.com>
2024-05-07 16:59:10 +0800

    Начало создания универсальной функции по созданию Embed

--- bot/commands/base.py
+++ bot/commands/base.py
@@ -1,12 +1,27 @@
 from functools import wraps
 from typing import Dict
+import json
 
-from discord import Interaction
+from discord import Interaction, Member, Embed
+
+from api.test_api_embed import EmbedAPIClient
 
 
 def example(interaction: Interaction, *args, **kwargs) -> Dict:
     pass
 
+
+async def create_embed(name: str) -> Embed:
+    """
+    Получает по API шаблон Embed-а и создает объект Embed.
+    name - имя Embed
+    """
+    async with EmbedAPIClient() as embed_api:
+        data = await embed_api.get_embed(name)
+
+    row_embed1 = data["embed_template"]
+    dict_row = json.loads(row_embed1)
+    return Embed.from_dict(dict_row)
 
 
 def command_custom(func):
@@ -25,7 +40,6 @@ def command_custom(func):
         await interaction.response.defer(ephemeral=False, thinking=True)
 
         msg_data = await func(interaction, *args, **kwargs)
-
         await interaction.edit_original_response(**msg_data)
 
     return in_func
